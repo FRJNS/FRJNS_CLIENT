72240ccfd4534e04e6dfa56f1dafddb2e846781f
Hyesung Oh <haesungoh414@gmail.com>
2023-04-11 23:26:54 +0900

    feat: video components

---
+++ src/components/videos/Video.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Window, WindowHeader } from "react95";
+import dynamic from "next/dynamic";
+import styled from "styled-components";
+
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+
+import { useRecoilValue } from "recoil";
+
+import { type Video } from "@/hooks/api/useGetVideosInfinite";
+import { volumeState } from "@/store/volume";
+
+export type Props = Omit<Video, "id">;
+
+export default function Video({ url, title }: Props) {
+  const { isPlaying, setTarget } = useAutoPlay();
+  const volume = useRecoilValue(volumeState);
+
+  return (
+    <StyledWrapper>
+      <StyledWindow ref={setTarget}>
+        <WindowHeader style={{ overflow: "scroll", width: "100%" }}>{title}</WindowHeader>
+
+        <ReactPlayer
+          url={url}
+          width="100%"
+          height="100%"
+          style={{ aspectRatio: "9 / 10" }}
+          playing={isPlaying}
+          controls={false}
+          volume={volume}
+          loop
+        />
+      </StyledWindow>
+    </StyledWrapper>
+  );
+}
+
+const StyledWrapper = styled.article`
+  scroll-snap-align: center;
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
+  padding: 4px 6px;
+`;
+
+const StyledWindow = styled(Window)`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+function useAutoPlay() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const { setTarget } = useIntersectionObserver({
+    onIntersect: ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        setIsPlaying(true);
+      } else {
+        setIsPlaying(false);
+      }
+    },
+  });
+
+  return { isPlaying, setTarget };
+}
